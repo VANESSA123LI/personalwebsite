f329dbba85322f4a087ad73a56464ffc87fca4aa
Vanessa Li <62302772+VANESSA123LI@users.noreply.github.com>
2026-03-15 11:47:14 -0700

    no right blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,57 +1,57 @@
 import Link from "next/link";
 
 const worldPosts = [
-  { 
+  {
     slug: "world-1",
-    title: "FTX and the future of crypto", 
+    title: "FTX and the future of crypto",
     date: "December 6, 2022"
   },
-  { 
+  {
     slug: "world-2",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "January 30, 2026"
   },
-  { 
+  {
     slug: "world-3",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "February 10, 2026"
   },
-  { 
+  {
     slug: "world-4",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "February 20, 2026"
   },
-  { 
+  {
     slug: "world-5",
-    title: "CComing soon", 
+    title: "CComing soon",
     date: "March 1, 2026"
   },
 ];
 
 const personalPosts = [
-  { 
+  {
     slug: "personal-1",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "January 15, 2026"
   },
-  { 
+  {
     slug: "personal-2",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "January 25, 2026"
   },
-  { 
+  {
     slug: "personal-3",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "February 5, 2026"
   },
-  { 
+  {
     slug: "personal-4",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "February 15, 2026"
   },
-  { 
+  {
     slug: "personal-5",
-    title: "Coming soon", 
+    title: "Coming soon",
     date: "February 25, 2026"
   },
 ];
@@ -60,9 +60,10 @@ export default function Blog() {
   return (
     <main className="relative mx-auto max-w-2xl px-6 py-12">
       <div className="flex justify-between items-baseline mb-12">
-      <h1 className="absolute right top-6 text-2xl font-bold">Blog</h1>
-        <Link href="/" className="text-lg">Home</Link>
+      <Link href="/" className="text-lg">Home</Link>
       </div>
+      <div>  <h1 className="text-2xl font-bold">Writings</h1></div>
+    
 
       <div className="space-y-12">
         {/* Personal Category */}
