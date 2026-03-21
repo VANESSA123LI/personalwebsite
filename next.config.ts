cb51572c052aaf5a9b13124501513075d919664b
Vanessa Li <vanessa.li@harvey.ai>
2026-03-21 14:55:28 -0700

    text and sizes for The Future of US Regulations on Generative AI

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "substack-post-media.s3.amazonaws.com",
+        pathname: "/public/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "substackcdn.com",
+        pathname: "/image/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
